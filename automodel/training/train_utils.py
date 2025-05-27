6b7e24929faee979263972240f7b4bf2dc90bc46
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-05-27 12:10:11 -0700

    fix import
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- automodel/training/train_utils.py
+++ automodel/training/train_utils.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
 from datetime import datetime
-from typing import Optional
 
 import torch
 
