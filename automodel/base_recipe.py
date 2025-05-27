692f9af6dfaef8205ba0a29bda43415dabb44a36
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-05-27 11:40:03 -0700

    update docsring
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- automodel/base_recipe.py
+++ automodel/base_recipe.py
@@ -11,13 +11,23 @@ class BaseRecipe:
     """
     Checkpoint registry
     """
-    def __setattr__(self, name: str, value):
+    def __setattr__(self, key, value):
+        """ Overriden __setattr__ to keep track of stateful classes.
+
+        Args:
+            key (str): attribute named.
+            value (Any): Value assigned
+
+        Raises:
+            ValueError: if __state_tracked is attemped to be overwriten.
+
+        """
         # assuming no one will do recipe.__dict__['__state_tracked'] = None
-        if name == '__state_tracked':
+        if key == '__state_tracked':
             raise ValueError("cannot set __state_tracked")
         if not '__state_tracked' in self.__dict__:
             self.__dict__['__state_tracked'] = set()
         if isinstance(value, (nn.Module, Optimizer)) or has_load_restore_state(value):
-            assert not name in self.__dict__['__state_tracked']
-            self.__dict__['__state_tracked'].add(name)
-        return super().__setattr__(name, value)
+            assert not key in self.__dict__['__state_tracked']
+            self.__dict__['__state_tracked'].add(key)
+        super().__setattr__(key, value)
