cfe5c64ee855b25ad2149641d8fd4ce44d1beff1
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-05-27 12:46:25 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- automodel/utils/dist_utils.py
+++ automodel/utils/dist_utils.py
@@ -13,9 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+import logging
 import os
+from datetime import datetime
+from typing import Any, Optional
+
+import torch
+import torch.distributed
 from contextlib import ContextDecorator
 import torch.distributed as dist
+import yaml
+
+from automodel.utils.yaml_utils import safe_yaml_representers
+
 
 
 class FirstRankPerNode(ContextDecorator):
@@ -86,30 +97,6 @@ class FirstRankPerNode(ContextDecorator):
             )
             return True
         return False
-# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import logging
-import os
-from datetime import datetime
-from typing import Any, Optional
-
-import torch
-import torch.distributed
-import yaml
-
-from automodel.utils.yaml_utils import safe_yaml_representers
 
 
 def get_rank_safe() -> int:
