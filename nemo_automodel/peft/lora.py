1ca7b1a6a675365c266d0c350e843658f09dc679
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-05-30 06:44:56 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/peft/lora.py
+++ nemo_automodel/peft/lora.py
@@ -1,6 +1,16 @@
-# lora_pt.py
-# Copyright (c) 2025 NVIDIA CORPORATION.
-# Apache 2.0 license.
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import math
 from dataclasses import dataclass, field
@@ -152,9 +162,6 @@ def merge_lora_weights(model: nn.Module):
     return model
 
 
-# -----------------------------------------------------------------------------#
-# 4.  Example                                                                  #
-# -----------------------------------------------------------------------------#
 if __name__ == "__main__":
     class ToyModel(nn.Module):
         def __init__(self):
