bb91a369f85d103e30f1205399b57137ec7717c6
Dong Hyuk Chang <donghyukc@nvidia.com>
2025-06-09 18:44:34 -0700

    Remove placeholder and add back functional test
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>

--- tests/functional_tests/L2_HF_Transformer_PEFT.sh
+++ tests/functional_tests/L2_HF_Transformer_PEFT.sh
@@ -13,4 +13,12 @@
 # limitations under the License.
 
 TRANSFORMERS_OFFLINE=1 coverage run -a --data-file=/workspace/.coverage --source=/workspace/nemo \
-tests/functional_tests/placeholder_functional.py
+recipes/llm/finetune.py \
+  --config recipes/llm/llama_3_2_1b_squad.yaml \
+  --model.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --step_scheduler.max_steps 3 \
+  --step_scheduler.grad_acc_steps 1 \
+  --dataset.tokenizer.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --validation_dataset.tokenizer.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
+  --dataset.dataset_name /home/TestData/lite/hf_cache/squad/ \
+  --dataset.limit_dataset_samples 10
