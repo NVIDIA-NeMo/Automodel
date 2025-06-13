071ffaef39c923a714e4aacc22c48ea845010bb1
adil-a <adil.asif2000@hotmail.com>
2025-06-13 10:45:29 -0700

    testing potential fix
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- tests/functional_tests/L2_DCP_Checkpoint.sh
+++ tests/functional_tests/L2_DCP_Checkpoint.sh
@@ -13,10 +13,8 @@
 # limitations under the License.
 
 export PYTHONPATH=${PYTHONPATH:-}:$(pwd)
-export CUDA_VISIBLE_DEVICES="0,1"
-export TRANSFORMERS_OFFLINE=1
 
-python -m torch.distributed.run --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode \
+TRANSFORMERS_OFFLINE=1 python -m torch.distributed.run --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode \
 -m pytest tests/functional_tests/checkpoint/test_dcp.py \
     --config recipes/llm/llama_3_2_1b_squad.yaml \
     --model.pretrained_model_name_or_path /home/TestData/akoumparouli/hf_mixtral_2l/ \
