0f9cbabe4c478f15dfdc68a031fb2ca2f821622d
oliver könig <okoenig@nvidia.com>
2025-06-16 21:19:20 +0000

    readd package info
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- nemo_automodel/__init__.py
+++ nemo_automodel/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import importlib
-from .__version__ import __version__
+from .package_info import __version__
 
 __all__ = [
     "_peft",
