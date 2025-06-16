55b5902d0b77c2d92d757a8ca1b9508f34b3460a
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-06-16 13:12:43 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/_transformers/__init__.py
+++ nemo_automodel/_transformers/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-from nemo_automodel._transformers.auto_model import NeMoAutoModelForCausalLM
+from nemo_automodel._transformers.auto_model import NeMoAutoModelForCausalLM, NeMoAutoModelForImageTextToText
 
-__all__ = ['NeMoAutoModelForCausalLM']
+__all__ = ["NeMoAutoModelForCausalLM", "NeMoAutoModelForImageTextToText"]
 
