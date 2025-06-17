4543730ef691375518e37f2b9019cf8c8a2b1f70
adil-a <adil.asif2000@hotmail.com>
2025-06-17 12:53:18 -0700

    moving torch backport file
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/checkpoint/__init__.py
+++ nemo_automodel/checkpoint/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ._torch_backports import apply_patches as _nemo__apply_patches
+_nemo__apply_patches()
+
