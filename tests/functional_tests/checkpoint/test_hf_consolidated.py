a28e79b26ba518af768e7f482fe848773db2eeec
adil-a <adil.asif2000@hotmail.com>
2025-06-17 07:20:53 +0000

    linting
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- tests/functional_tests/checkpoint/test_hf_consolidated.py
+++ tests/functional_tests/checkpoint/test_hf_consolidated.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: disable=line-too-long
 """Tests for DCP checkpointing."""
 
 import os
