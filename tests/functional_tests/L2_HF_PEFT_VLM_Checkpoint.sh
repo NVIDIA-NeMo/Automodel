7578e1dd6ee7899e16e83a043178bcdea56f7ec2
HuiyingLi <willwin.lee@gmail.com>
2025-07-12 23:02:50 -0700

    change dataset
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- tests/functional_tests/L2_HF_PEFT_VLM_Checkpoint.sh
+++ tests/functional_tests/L2_HF_PEFT_VLM_Checkpoint.sh
@@ -46,9 +46,11 @@ TRANSFORMERS_OFFLINE=1 python -m torch.distributed.run --nproc_per_node=2 --nnod
   --model.pretrained_model_name_or_path /home/TestData/huiyingl/hf_gemma3_2l/ \
   --step_scheduler.max_steps 10 \
   --step_scheduler.grad_acc_steps 4 \
-  --dataset._target_=nemo_automodel.datasets.vlm.datasets.make_rdr_dataset \
-  --dataset.path_or_dataset /home/TestData/lite/hf_cache/rdr/ \
-  --dataset.limit_dataset_samples 1000 \
+  --dataset._target_=nemo_automodel.datasets.vlm.datasets.make_cord_v2_dataset \
+  --dataset.path_or_dataset /home/TestData/lite/hf_cache/mini_cord_v2/ \
+  --dataset.limit_dataset_samples 100 \
+  --validation_dataset.path_or_dataset my/mini_cord_v2/ \
+  --validation_dataset.limit_dataset_samples 10 \
   --step_scheduler.ckpt_every_steps 10 \
   --checkpoint.enabled true \
   --checkpoint.checkpoint_dir checkpoints/ \
--- tests/functional_tests/L2_HF_Transformer_VLM_SFT.sh
+++ tests/functional_tests/L2_HF_Transformer_VLM_SFT.sh
@@ -18,6 +18,8 @@ recipes/vlm/finetune.py \
   --model.pretrained_model_name_or_path /home/TestData/huiyingl/hf_gemma3_2l/ \
   --step_scheduler.max_steps 3 \
   --step_scheduler.grad_acc_steps 1 \
-  --dataset._target_=nemo_automodel.datasets.vlm.datasets.make_rdr_dataset \
-  --dataset.path_or_dataset /home/TestData/lite/hf_cache/rdr/ \
-  --dataset.limit_dataset_samples 10
+  --dataset._target_=nemo_automodel.datasets.vlm.datasets.make_cord_v2_dataset \
+  --dataset.path_or_dataset /home/TestData/lite/hf_cache/mini_cord_v2/ \
+  --dataset.limit_dataset_samples 100 \
+  --validation_dataset.path_or_dataset my/mini_cord_v2/ \
+  --validation_dataset.limit_dataset_samples 10 \
