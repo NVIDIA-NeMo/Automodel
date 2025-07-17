46e3398b835773a0c8e8f960b1fc5488c7bf1cba
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-07-17 16:06:13 -0700

    revert
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/recipes/llm/finetune.py
+++ nemo_automodel/recipes/llm/finetune.py
@@ -329,8 +329,6 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
             seed=self.cfg.get("seed", 42),
             tp_size=self.cfg.get("distributed.tp_size", 1),
         )
-        print(self.model)
-
         self.loss_fn = build_loss_fn(self.dist_env.device, self.cfg.loss_fn)
         self.dataloader = build_dataloader(
             self.cfg.dataset,
