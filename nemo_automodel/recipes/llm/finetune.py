4b4635128b7746c62129011314935beb8b2e51a6
HuiyingLi <willwin.lee@gmail.com>
2025-07-22 12:05:33 -0700

    format
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/recipes/llm/finetune.py
+++ nemo_automodel/recipes/llm/finetune.py
@@ -39,8 +39,8 @@ from nemo_automodel.components.distributed.init_utils import initialize_distribu
 from nemo_automodel.components.distributed.nvfsdp import NVFSDPManager
 from nemo_automodel.components.loggers.log_utils import setup_logging
 from nemo_automodel.components.loggers.wandb_utils import suppress_wandb_log_messages
-from nemo_automodel.components.optim.scheduler import OptimizerParamScheduler
 from nemo_automodel.components.loss.linear_ce import FusedLinearCrossEntropy
+from nemo_automodel.components.optim.scheduler import OptimizerParamScheduler
 from nemo_automodel.components.training.base_recipe import BaseRecipe
 from nemo_automodel.components.training.rng import StatefulRNG
 from nemo_automodel.components.training.step_scheduler import StepScheduler
