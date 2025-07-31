1d14a73b03e0d66252a2b8a05f908f5514a6560f
HuiyingLi <willwin.lee@gmail.com>
2025-07-31 15:58:15 -0700

    copyright
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- tests/unit_tests/quantization/test_fp8.py
+++ tests/unit_tests/quantization/test_fp8.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-"""
-Unit tests for FP8 quantization functionality.
-"""
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import pytest
 import torch
@@ -12,7 +21,6 @@ from nemo_automodel.components.quantization.fp8 import (
     FP8Config,
     apply_fp8_to_model,
     verify_fp8_conversion,
-    _has_cuda_capability,
     _module_filter_fn,
 )
 
