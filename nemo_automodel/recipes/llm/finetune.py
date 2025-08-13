e22fef60210aa9695fbe35ac2ed15ab819109327
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-08-13 10:28:11 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/recipes/llm/finetune.py
+++ nemo_automodel/recipes/llm/finetune.py
@@ -45,8 +45,8 @@ from nemo_automodel.components.optim.scheduler import OptimizerParamScheduler
 from nemo_automodel.components.training.rng import StatefulRNG
 from nemo_automodel.components.training.step_scheduler import StepScheduler
 from nemo_automodel.components.training.utils import count_tail_padding
-from nemo_automodel.components.utils.model_utils import print_trainable_parameters
 from nemo_automodel.components.utils.dist_utils import get_sync_ctx
+from nemo_automodel.components.utils.model_utils import print_trainable_parameters
 from nemo_automodel.recipes.base_recipe import BaseRecipe
 
 if TYPE_CHECKING:
