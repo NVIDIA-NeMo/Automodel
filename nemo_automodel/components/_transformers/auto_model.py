dff1ced0b0eb6c248327370721a32cb54ef10f20
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-08-15 11:41:45 -0700

    change default to sdpa for reproducibility
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/components/_transformers/auto_model.py
+++ nemo_automodel/components/_transformers/auto_model.py
@@ -177,7 +177,7 @@ class _BaseNeMoAutoModelClass(_BaseAutoModelClass):
         use_sdpa_patching: bool = True,
         sdpa_method: Optional[List[SDPBackend]] = None,
         torch_dtype="auto",
-        attn_implementation: str = "flash_attention_2",
+        attn_implementation: str = "sdpa",
         fp8_config: Optional[object] = None,
         **kwargs,
     ) -> PreTrainedModel:
