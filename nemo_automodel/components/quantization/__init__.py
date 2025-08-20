5c0e22451eb6e30fb6aaec1e3f743f09cc6becdb
HuiyingLi <willwin.lee@gmail.com>
2025-08-20 00:00:08 -0700

    format
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/components/quantization/__init__.py
+++ nemo_automodel/components/quantization/__init__.py
@@ -1,10 +1,10 @@
 from .fp8 import (
-    HAVE_TORCHAO, 
-    FP8Config, 
-    apply_fp8_to_model, 
-    verify_fp8_conversion,
+    HAVE_TORCHAO,
+    FP8Config,
+    apply_fp8_to_model,
     build_fp8_config,
     create_fp8_config_from_dict,
+    verify_fp8_conversion,
 )
 
 if HAVE_TORCHAO:
@@ -13,12 +13,12 @@ else:
     Float8LinearConfig = None
 
 __all__ = [
-    "apply_fp8_to_model", 
-    "verify_fp8_conversion", 
+    "apply_fp8_to_model",
+    "verify_fp8_conversion",
     "build_fp8_config",
     "create_fp8_config_from_dict",
-    "HAVE_TORCHAO", 
-    "FP8Config"
+    "HAVE_TORCHAO",
+    "FP8Config",
 ]
 
 if HAVE_TORCHAO:
