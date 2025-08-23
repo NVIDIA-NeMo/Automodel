2d26b184e95873db01bf46a6fbd887c68f5dc4be
adil-a <adil.asif2000@hotmail.com>
2025-08-23 07:25:10 +0000

    linting
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/recipes/llm/finetune.py
+++ nemo_automodel/recipes/llm/finetune.py
@@ -553,9 +553,7 @@ class FinetuneRecipeForNextTokenPrediction(BaseRecipe):
             )
 
         # Scheduler
-        self.step_scheduler = build_step_scheduler(
-            self.cfg.get("step_scheduler", None), self.dataloader
-        )
+        self.step_scheduler = build_step_scheduler(self.cfg.get("step_scheduler", None), self.dataloader)
 
         # Build learning rate scheduler
         self.lr_scheduler = build_lr_scheduler(self.cfg.get("lr_scheduler", None), self.optimizer, self.step_scheduler)
