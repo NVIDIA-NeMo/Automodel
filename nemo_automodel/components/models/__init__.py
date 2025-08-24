ef61620b552500da736b8ee2e600f787e8e25e84
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-08-23 19:09:44 -0700

    fix
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/components/models/__init__.py
+++ nemo_automodel/components/models/__init__.py
@@ -16,8 +16,9 @@
 Currently includes:
     • build_gpt2_model – returns a GPT-2 causal language model (Flash-Attention-2 by default).
 """
+
 from .gpt2 import build_gpt2_model  # noqa: F401
 
 __all__ = [
     "build_gpt2_model",
-] 
+]
