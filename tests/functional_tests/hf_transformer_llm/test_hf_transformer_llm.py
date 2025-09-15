bcb788cde88e60e1cc5b6773aba2b17092a8f458
adil-a <adil.asif2000@hotmail.com>
2025-09-15 07:24:46 +0000

    adding ddp test
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
+++ tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
@@ -21,6 +21,7 @@ HF_TRANSFORMER_LLM_FSDP2_TP2_FILENAME = "L2_HF_Transformer_LLM_FSDP2_TP2.sh"
 HF_TRANSFORMER_LLM_FSDP2_TP2_HF_TPPLAN_FILENAME = "L2_HF_Transformer_LLM_FSDP2_TP2_HF_TPPLAN.sh"
 HF_TRANSFORMER_LLM_MegatronFSDP_TP2_FILENAME = "L2_HF_Transformer_LLM_MegatronFSDP_TP2.sh"
 HF_TRANSFORMER_LLM_MegatronFSDP_TP2_HF_TPPLAN_FILENAME = "L2_HF_Transformer_LLM_MegatronFSDP_TP2_HF_TPPLAN.sh"
+HF_TRANSFORMER_LLM_DDP_FILENAME = "L2_HF_Transformer_LLM_DDP.sh"
 
 
 
@@ -40,3 +41,7 @@ class TestHFTransformerLLM:
     @pytest.mark.pleasefixme
     def test_hf_transformer_llm_megatron_fsdp_tp2_hf_tpplan(self):
         run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_MegatronFSDP_TP2_HF_TPPLAN_FILENAME)
+
+    @pytest.mark.pleasefixme
+    def test_hf_transformer_llm_ddp(self):
+        run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_DDP_FILENAME)
