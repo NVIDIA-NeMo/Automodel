ab0c52b3fc3c409fdc60f3d32b616489536be4e7
HuiyingLi <willwin.lee@gmail.com>
2025-09-22 23:08:04 -0700

    fix
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/recipes/llm/train_ft.py
+++ nemo_automodel/recipes/llm/train_ft.py
@@ -40,7 +40,6 @@ from wandb import Settings
 from nemo_automodel.components._peft.lora import apply_lora_to_linear_modules
 from nemo_automodel.components._transformers.utils import apply_cache_compatibility_patches
 from nemo_automodel.components.checkpoint.checkpointing import CheckpointingConfig, load_model_from_base_checkpoint
-from nemo_automodel.components.checkpoint.checkpointing import initialize_model_randomly
 from nemo_automodel.components.config._arg_parser import parse_args_and_load_config
 from nemo_automodel.components.datasets.llm.megatron.sampler import create_megatron_sampler
 from nemo_automodel.components.datasets.llm.megatron_dataset import MegatronPretraining
