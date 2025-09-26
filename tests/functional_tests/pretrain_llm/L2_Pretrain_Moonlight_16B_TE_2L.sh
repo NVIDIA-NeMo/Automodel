f23a123418df98c34c7bbbaced46ede6fdb0296a
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-09-25 23:58:32 -0700

    add 2-layer moonlight functional test
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/functional_tests/pretrain_llm/L2_Pretrain_Moonlight_16B_TE_2L.sh
+++ tests/functional_tests/pretrain_llm/L2_Pretrain_Moonlight_16B_TE_2L.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tests.utils.test_utils import run_test_script
+#!/bin/bash
+set -xeuo pipefail # Exit immediately if a command exits with a non-zero status
 
-TEST_FOLDER = "pretrain_llm"
+export PYTHONPATH=${PYTHONPATH:-}:$(pwd)
+export CUDA_VISIBLE_DEVICES="0,1"
 
-class TestPretrainLLM:
-    def test_pretrain(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain.sh")
-
-    def test_pretrain_hf(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain_HF.sh")
-
-    def test_pretrain_moonlight_16b_te_2l(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain_Moonlight_16B_TE_2L.sh")
+TRANSFORMERS_OFFLINE=1 python -m torch.distributed.run \
+--nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode \
+examples/llm_pretrain/pretrain.py \
+  --config tests/functional_tests/pretrain_llm/moonlight_16b_te_2l.yaml
