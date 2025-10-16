0be0305744676abdb097119a4caa002268202a05
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2025-10-16 03:19:08 -0700

    fix imports
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/recipes/vlm/finetune.py
+++ nemo_automodel/recipes/vlm/finetune.py
@@ -34,7 +34,6 @@ from wandb import Settings
 
 from nemo_automodel._transformers.utils import apply_cache_compatibility_patches
 from nemo_automodel.components._peft.lora import apply_lora_to_linear_modules
-from nemo_automodel._transformers.utils import apply_cache_compatibility_patches
 from nemo_automodel.components.checkpoint.checkpointing import Checkpointer, CheckpointingConfig
 from nemo_automodel.components.config._arg_parser import parse_args_and_load_config
 from nemo_automodel.components.datasets.vlm.collate_fns import COLLATE_FNS
