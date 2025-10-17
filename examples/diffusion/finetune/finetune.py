01f8f5c559636d88d3955c96b52beae72158e66f
Pranav Prashant Thombre <pthombre@nvidia.com>
2025-10-17 12:15:43 -0700

    Removing experimental code from examples directory
    Signed-off-by: Pranav Prashant Thombre <pthombre@nvidia.com>

--- examples/diffusion/finetune/finetune.py
+++ examples/diffusion/finetune/finetune.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from nemo_automodel.components.config._arg_parser import parse_args_and_load_config
-from nemo_automodel.recipes.diffusion.train_wan21 import TrainWan21DiffusionRecipe
+from nemo_automodel.recipes.diffusion.finetune import TrainWan21DiffusionRecipe
 
 
 def main(default_config_path="examples/diffusion/finetune/wan2_1_t2v_flow.yaml"):
