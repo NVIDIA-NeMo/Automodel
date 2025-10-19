1489d632539e37e04b858431d99bac4a0c68f71b
HuiyingLi <willwin.lee@gmail.com>
2025-10-19 14:01:38 -0700

    lint
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/components/datasets/llm/__init__.py
+++ nemo_automodel/components/datasets/llm/__init__.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .chat_dataset import ChatDataset  # noqa: F401
 from .column_mapped_text_instruction_dataset import ColumnMappedTextInstructionDataset  # noqa: F401
 from .nanogpt_dataset import NanogptDataset  # noqa: F401
 from .squad import make_squad_dataset  # noqa: F401
-from .chat_dataset import ChatDataset  # noqa: F401
 
 __all__ = [
     "NanogptDataset",
