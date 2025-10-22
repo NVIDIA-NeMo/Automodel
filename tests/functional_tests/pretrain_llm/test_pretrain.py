32d6e4961a14cea9706d9f4a0cb1bca9efe86c01
Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
2025-10-21 17:31:12 -0700

    fix: enable checkpoint saving by default (#661)
    * enable checkpoint saving by default
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    
    ---------
    
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>
    Co-authored-by: adil-a <adil.asif2000@hotmail.com>

--- tests/functional_tests/pretrain_llm/test_pretrain.py
+++ tests/functional_tests/pretrain_llm/test_pretrain.py
@@ -13,15 +13,28 @@
 # limitations under the License.
 
 from tests.utils.test_utils import run_test_script
+import shutil
 
 TEST_FOLDER = "pretrain_llm"
 
 class TestPretrainLLM:
     def test_pretrain(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain.sh")
+        try:
+            run_test_script(TEST_FOLDER, "L2_Pretrain.sh")
+        finally:
+            # remove the checkpoint directory
+            shutil.rmtree("checkpoints/", ignore_errors=True)
 
     def test_pretrain_hf(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain_HF.sh")
+        try:
+            run_test_script(TEST_FOLDER, "L2_Pretrain_HF.sh")
+        finally:
+            # remove the checkpoint directory
+            shutil.rmtree("checkpoints/", ignore_errors=True)
 
     def test_pretrain_moonlight_16b_te_2l(self):
-        run_test_script(TEST_FOLDER, "L2_Pretrain_Moonlight_16B_TE_2L.sh")
+        try:
+            run_test_script(TEST_FOLDER, "L2_Pretrain_Moonlight_16B_TE_2L.sh")
+        finally:
+            # remove the checkpoint directory
+            shutil.rmtree("checkpoints/", ignore_errors=True)
