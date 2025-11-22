c3d4f9b876c99b519d90d996409e2004aefa0809
HuiyingLi <willwin.lee@gmail.com>
2025-11-22 01:00:35 -0800

    set attn param
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/components/attention/utils.py
+++ nemo_automodel/components/attention/utils.py
@@ -141,9 +141,11 @@ def preprocess_args_and_kwargs_for_attn(
         q = q.transpose(1, 2).contiguous()
         k = k.transpose(1, 2).contiguous()
         v = v.transpose(1, 2).contiguous()
-        attn_kwargs["is_causal"] = True
         if attention_mask is not None and (attention_mask.dtype == torch.bool or attention_mask.is_floating_point()):
             attn_kwargs["attn_mask"] = attention_mask
+            attn_kwargs["is_causal"] = False
+        else:
+            attn_kwargs["is_causal"] = True
 
     return q, k, v, attn_kwargs
 
