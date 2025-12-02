8c379d7597bb9fb0a9b52504ce7f614cdf4fb44e
adil-a <adil.asif2000@hotmail.com>
2025-12-02 20:59:18 +0000

    hf home
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/recipes/llm/train_ft.py
+++ nemo_automodel/recipes/llm/train_ft.py
@@ -81,6 +81,7 @@ from transformers import AutoConfig
 from transformers.modeling_utils import no_init_weights
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from transformers.utils import ContextManagers
+from huggingface_hub import constants
 
 if TYPE_CHECKING:
     from torch.optim import Optimizer
@@ -256,7 +257,7 @@ def build_model_and_optimizer(
                 checkpointer.load_base_model(
                     mp,
                     device,
-                    cfg_model.get("cache_dir", os.environ["HF_HOME"]),
+                    cfg_model.get("cache_dir", os.environ.get("HF_HOME", constants.HF_HOME)),
                     _get_model_name(cfg_model),
                     getattr(cfg_peft, "lora_A_init", None),
                     load_base_model=load_base_model,
@@ -316,7 +317,7 @@ def build_model_and_optimizer(
             checkpointer.load_base_model(
                 model,
                 device,
-                cfg_model.get("cache_dir", os.environ["HF_HOME"]),
+                cfg_model.get("cache_dir", os.environ.get("HF_HOME", constants.HF_HOME)),
                 _get_model_name(cfg_model),
                 getattr(cfg_peft, "lora_A_init", None),
                 load_base_model=load_base_model,
@@ -367,7 +368,7 @@ def build_checkpoint_config(cfg_ckpt, cache_dir, model_repo_id, is_peft) -> Chec
         checkpoint_dir="checkpoints/",
         model_save_format="safetensors",
         model_repo_id=model_repo_id,
-        model_cache_dir=cache_dir if cache_dir is not None else os.environ["HF_HOME"],
+        model_cache_dir=cache_dir if cache_dir is not None else os.environ.get("HF_HOME", constants.HF_HOME),
         save_consolidated=True,
         is_peft=is_peft,
     )
