66827a582540db20a093d40899fdbf20797f279f
Dong Hyuk Chang <thomaschang26@tutanota.com>
2025-12-03 13:22:58 -0500

    ci: Update transformers to latest version 4.57.3 (#890)
    Signed-off-by: gitlab-runner <gitlab-runner@gitlab-master.nvidia.com>
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>
    Co-authored-by: gitlab-runner <gitlab-runner@gitlab-master.nvidia.com>
    Co-authored-by: HuiyingLi <willwin.lee@gmail.com>
    Co-authored-by: Alexandros Koumparoulis <153118171+akoumpa@users.noreply.github.com>
    Co-authored-by: Zhiyu Li <zhiyul@NVIDIA.com>

--- tests/functional_tests/hf_transformer_finetune/L2_HF_Transformer_PEFT_Benchmark_Llama_custom.sh
+++ tests/functional_tests/hf_transformer_finetune/L2_HF_Transformer_PEFT_Benchmark_Llama_custom.sh
@@ -18,11 +18,14 @@ set -xeuo pipefail # Exit immediately if a command exits with a non-zero status
 export PYTHONPATH=${PYTHONPATH:-}:$(pwd)
 export CUDA_VISIBLE_DEVICES="0,1"
 
+# revert back to meta-llama/Llama-3.2-1B when we can use cache from HF_HOME
+MODEL_PATH=/home/TestData/HF_HOME/hub/models--meta-llama--Llama-3.2-1B/snapshots/4e20de362430cd3b72f300e6b0f18e50e7166e08/
+
 # override with a smaller model meta-llama/Llama-3.2-1B for testing
 TRANSFORMERS_OFFLINE=1 python -m torch.distributed.run --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode \
 nemo_automodel/recipes/llm/benchmark.py \
     --config examples/llm_finetune/llama3_3/custom_llama3_3_70b_instruct_peft_benchmark.yaml \
-    --model.pretrained_model_name_or_path=meta-llama/Llama-3.2-1B \
+    --model.pretrained_model_name_or_path=${MODEL_PATH} \
     --distributed.tp_size=2 \
     --distributed.pp_size=1 \
     --distributed.sequence_parallel=False \
