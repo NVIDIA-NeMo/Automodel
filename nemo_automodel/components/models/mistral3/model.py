9b4908c50d05031d08b460b734a9eea91d77dd99
HuiyingLi <willwin.lee@gmail.com>
2025-12-05 01:07:33 -0800

    update copyright
    Signed-off-by: HuiyingLi <willwin.lee@gmail.com>

--- nemo_automodel/components/models/mistral3/model.py
+++ nemo_automodel/components/models/mistral3/model.py
@@ -1,14 +1,16 @@
-"""
-Custom Mistral3 integration for NeMo Automodel.
-
-This module:
-- Imports the VLM wrapper `Mistral3ForConditionalGeneration` from transformers_bak.
-- Vendors the text-only Ministral3 config/model so the registry can resolve the
-  decoder without relying on transformers_5.
-- Patches AutoModel.from_config to return the vendored Ministral3Model when
-  config.model_type == "ministral3".
-- Exposes ModelClass for the NeMo ModelRegistry.
-"""
+# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
 import types
