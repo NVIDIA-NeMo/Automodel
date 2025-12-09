c2961b328db1b0ce2426b28d14699f43bc10efe5
Zhiyu Li <zhiyul@NVIDIA.com>
2025-12-09 14:33:23 -0800

    fix
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>

--- nemo_automodel/autonvtx/__init__.py
+++ nemo_automodel/autonvtx/__init__.py
@@ -1,12 +1,23 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import torch
-import sys
-import functools
-import types
-import os
 from threading import local
 
-# Debug flag - set NVTX_PATCH_DEBUG=1 to enable logging
-_DEBUG = os.environ.get('NVTX_PATCH_DEBUG', '0') == '1'
+
+# inspired by https://github.com/zasdfgbnm/autonvtx
 
 # Thread-local storage to track active NVTX ranges and prevent recursion
 _thread_local = local()
