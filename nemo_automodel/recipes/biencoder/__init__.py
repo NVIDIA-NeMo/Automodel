b1e9a3e58afd09ef1744a5023505c03e33f4fd63
Radek Osmulski <rosmulski@gmail.com>
2026-01-21 06:56:16 +1000

    feat: Implement hard negative mining (#1069)
    Signed-off-by: Radek Osmulski <rosmulski@gmail.com>
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>
    Co-authored-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/recipes/biencoder/__init__.py
+++ nemo_automodel/recipes/biencoder/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .mine_hard_negatives import MineHardNegativesRecipe
 from .train_biencoder import TrainBiencoderRecipe
 
-__all__ = ["TrainBiencoderRecipe"]
+__all__ = ["MineHardNegativesRecipe", "TrainBiencoderRecipe"]
