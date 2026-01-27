9903b34e38cb30a0e9d70669052a81dbafb6c0cd
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2026-01-27 08:27:40 -0800

    fix import
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/recipes/llm/train_ft.py
+++ nemo_automodel/recipes/llm/train_ft.py
@@ -31,7 +31,8 @@ from torch.utils.data import DataLoader, IterableDataset
 from torchao.float8 import precompute_float8_dynamic_scale_for_fsdp
 from torchdata.stateful_dataloader.sampler import StatefulDistributedSampler
 from transformers import AutoConfig
-from transformers.modeling_utils import no_init_weights
+# from transformers.modeling_utils import no_init_weights
+from transformers.initialization import no_init_weights
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from transformers.utils import ContextManagers
 from wandb import Settings
