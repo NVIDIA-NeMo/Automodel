68808d34990ef94f72c8ef251af0bf96c5ffe0b4
adil-a <adil.asif2000@hotmail.com>
2026-01-28 02:52:37 -0800

    flag for expert score bias creation
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/components/models/nemotron_v3/model.py
+++ nemo_automodel/components/models/nemotron_v3/model.py
@@ -71,6 +71,7 @@ class NemotronV3Model(nn.Module):
             shared_expert_gate=False,
             shared_expert_inter_dim=config.moe_shared_expert_intermediate_size,
             shared_expert_activation="relu2",  # Use ReLU² for shared experts
+            force_e_score_correction_bias=True,  # NemotronV3 checkpoint has this buffer
         )
 
         # Embeddings
