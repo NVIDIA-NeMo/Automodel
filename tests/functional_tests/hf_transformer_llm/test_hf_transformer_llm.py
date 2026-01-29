799978597b4aa4eb5b5d5afb367931f3d70a14f5
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2026-01-29 12:12:32 -0800

    re-enable tests
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
+++ tests/functional_tests/hf_transformer_llm/test_hf_transformer_llm.py
@@ -31,15 +31,15 @@ class TestHFTransformerLLM:
     @pytest.mark.pleasefixme
     def test_hf_transformer_llm_fsdp2_tp2(self):
         run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_FSDP2_TP2_FILENAME)
-    
+
     @pytest.mark.pleasefixme
     def test_hf_transformer_llm_fsdp2_tp2_hf_tpplan(self):
         run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_FSDP2_TP2_HF_TPPLAN_FILENAME)
 
-    # @pytest.mark.pleasefixme
-    # def test_hf_transformer_llm_megatron_fsdp_tp2(self):
-    #     run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_MegatronFSDP_TP2_FILENAME)
-    
-    # @pytest.mark.pleasefixme
-    # def test_hf_transformer_llm_megatron_fsdp_tp2_hf_tpplan(self):
-    #     run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_MegatronFSDP_TP2_HF_TPPLAN_FILENAME)
+    @pytest.mark.pleasefixme
+    def test_hf_transformer_llm_megatron_fsdp_tp2(self):
+        run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_MegatronFSDP_TP2_FILENAME)
+
+    @pytest.mark.pleasefixme
+    def test_hf_transformer_llm_megatron_fsdp_tp2_hf_tpplan(self):
+        run_test_script(TEST_FOLDER, HF_TRANSFORMER_LLM_MegatronFSDP_TP2_HF_TPPLAN_FILENAME)
