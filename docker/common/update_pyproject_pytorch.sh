5234f4c7c488dc31379d674a8696f0cf3c152e5c
Dong Hyuk Chang <thomaschang26@tutanota.com>
2026-02-03 12:25:06 -0500

    ci: Add doc-strings to update_pyproject_pytorch.sh (#1155)
    Signed-off-by: Dong Hyuk Chang <donghyukc@nvidia.com>

--- docker/common/update_pyproject_pytorch.sh
+++ docker/common/update_pyproject_pytorch.sh
@@ -1,4 +1,29 @@
+# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/usr/bin/env bash
+
+#######################################
+##
+## Script to update pyproject.toml and uv.lock.
+## Updates uv configuration with override dependnecies necessary for working with PyTorch base image.
+##
+## Input: Absolute path to Automodel directory
+##
+## Example: bash docker/common/update_pyproject_pytorch.sh /opt/Automodel
+##
+########################################
 
 # Exit on error and undefined variables
 set -euo pipefail
