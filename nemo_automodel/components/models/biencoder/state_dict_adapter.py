24bb82d21fc95f4f9fad183a3effc5da44cc7b17
adil-a <adil.asif2000@hotmail.com>
2026-02-04 18:18:43 +0000

    remoev
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_automodel/components/models/biencoder/state_dict_adapter.py
+++
@@ -1,6 +0,0 @@
-# This file has been moved to components/models/common/bidirectional.py
-# Please delete this file.
-raise ImportError(
-    "This module has been moved. "
-    "Please import from nemo_automodel.components.models.common.bidirectional instead."
-)
