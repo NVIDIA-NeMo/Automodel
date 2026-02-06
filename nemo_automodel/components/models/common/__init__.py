f33e2e35f0b940d06c50c9f2f3c57109726b9d66
Zhiyu Li <zhiyul@NVIDIA.com>
2026-02-06 17:17:08 +0800

    chore: refactor common te module (#1172)
    * chore: refactor common te module
    
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>
    
    * chore: refactor common te module
    
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>
    
    * fix test
    
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>
    
    ---------
    
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>

--- nemo_automodel/components/models/common/__init__.py
+++ nemo_automodel/components/models/common/__init__.py
@@ -22,15 +22,12 @@ from nemo_automodel.components.models.common.combined_projection.state_dict_adap
     CombinedProjectionStateDictAdapter,
 )
 from nemo_automodel.components.models.common.hf_checkpointing_mixin import HFCheckpointingMixin
-from nemo_automodel.components.models.common.te_utils import (
+from nemo_automodel.components.models.common.utils import (
     HAVE_DEEP_EP,
     HAVE_TE,
     BackendConfig,
-    TELinearWrapper,
-    TENormWrapper,
     initialize_linear_module,
     initialize_rms_norm_module,
-    is_tensor_unallocated,
 )
 
 __all__ = [
@@ -44,9 +41,6 @@ __all__ = [
     "HAVE_TE",
     "HAVE_DEEP_EP",
     "BackendConfig",
-    "is_tensor_unallocated",
-    "TENormWrapper",
-    "TELinearWrapper",
     "initialize_rms_norm_module",
     "initialize_linear_module",
 ]
