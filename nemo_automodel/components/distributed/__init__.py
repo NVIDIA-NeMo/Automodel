90ac37db60e4ba03f34e5dfe0484526b2ef644dc
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2026-02-08 15:58:42 -0800

    surface
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- nemo_automodel/components/distributed/__init__.py
+++ nemo_automodel/components/distributed/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from nemo_automodel.components.distributed.config import FSDP2Config, MegatronFSDPConfig, DDPConfig
+
+__all__ = ["FSDP2Config", "MegatronFSDPConfig", "DDPConfig"]
