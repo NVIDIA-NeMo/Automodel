4ef830beb8e88482582323db8d17fefc1d974dc9
Zhiyu Li <zhiyul@NVIDIA.com>
2026-02-10 11:42:59 +0800

    fix: temp walk around VocabParallelEmbedding to address OOM (#1197)
    [fix]: temp walk around VocabParallelEmbedding to address OOM
    
    Signed-off-by: Zhiyu Li <zhiyul@NVIDIA.com>

--- nemo_automodel/components/distributed/optimized_tp_plans.py
+++ nemo_automodel/components/distributed/optimized_tp_plans.py
@@ -217,7 +217,7 @@ def _parallelize_llama(
 ) -> dict[str, ParallelStyle]:
     """Parallelizes a LlamaForCausalLM model across data and tensor parallel dimensions."""
     base_model_tp_plan: dict[str, ParallelStyle] = {
-        "model.embed_tokens": VocabParallelEmbedding(input_layouts=Replicate()),
+        "model.embed_tokens": RowwiseParallel(input_layouts=Replicate()),
         "model.layers.*.self_attn.q_proj": ColwiseParallel(),
         "model.layers.*.self_attn.k_proj": ColwiseParallel(),
         "model.layers.*.self_attn.v_proj": ColwiseParallel(),
