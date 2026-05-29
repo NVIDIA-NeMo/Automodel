b86693b4c167c3a12e552b30f335cd43bc531b9e
Abhishree <abhishreetm@gmail.com>
2026-05-28 17:38:46 -0700

    fix(models): guard torchvision in step3p7 + add copyright headers
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- nemo_automodel/components/models/step3p7/processing_step3.py
+++ nemo_automodel/components/models/step3p7/processing_step3.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from itertools import product
 from math import ceil
 from typing import Literal, Optional, TypedDict, Union
@@ -5,12 +19,15 @@ from typing import Literal, Optional, TypedDict, Union
 import numpy as np
 import torch
 from PIL import Image
-from torchvision import transforms
-from torchvision.transforms.functional import InterpolationMode
 from transformers import BaseImageProcessor
 from transformers.feature_extraction_utils import BatchFeature, TensorType
 from transformers.image_utils import ImageInput
 from transformers.processing_utils import ProcessorMixin
+
+from nemo_automodel.shared.import_utils import safe_import_from
+
+_, transforms = safe_import_from("torchvision", "transforms")
+_, InterpolationMode = safe_import_from("torchvision.transforms.functional", "InterpolationMode")
 
 MAX_IMAGE_SIZE: int = 3024
 
